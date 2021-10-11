c3fa6680169e9796f9dece8ac580ed3971cbc5b0
Daniel Perebinos <danielperebinos>
2021-10-11 22:59:04 +0300

    Js theme changed

--- Laboratorul III/WebProject/scripts/theme.js
+++ Laboratorul III/WebProject/scripts/theme.js
@@ -1,88 +1,17 @@
 myStorage = window.localStorage;
 
-function whiteTheme(){
-  let header = document.querySelector('header');
-  let headerLinks = document.querySelectorAll('header a');
+function whiteTheme()
+{
   let body = document.querySelector('body');
-  let mainbox = document.querySelector('.mainbox');
-  let menu = document.querySelector('.menu');
-
-  header.classList.remove('dark__header');
-  body.classList.remove('dark__body');
-  mainbox.classList.remove('dark__mainbox');
-  menu.classList.remove('dark__header');
-
-  let link;
-  for(link of headerLinks){
-      link.classList.remove('dark__link');
-  }
-
-
-  if (location.pathname.split("/").slice(-1) == 'home.html')
-  {
-    let genrebox = document.querySelector('.genresbtn');
-    let genreb = document.querySelectorAll('.genrechoise');
-    let slider = document.querySelectorAll('.slider');
-
-    genrebox.classList.remove('dark__body');
-
-    for(link of genreb){
-      link.classList.remove('dark__link');
-    }
-  }
-  else if(location.pathname.split("/").slice(-1) == 'top.html')
-  {
-    let titlebox = document.querySelectorAll('.titlebox');
-
-    for(box of titlebox){
-      box.classList.add('titlebox__light');
-      box.classList.remove('dark__titlebox');
-    }
-  }
-
+  body.classList.remove('dark');
+  body.style.backgroundColor = 'white';
 }
 
-function darkTheme(){
-  let header = document.querySelector('header');
-  let headerLinks = document.querySelectorAll('header a');
+function darkTheme()
+{
   let body = document.querySelector('body');
-  let mainbox = document.querySelector('.mainbox');
-  let menu = document.querySelector('.menu');
-
-  header.classList.add('dark__header');
-  body.classList.add('dark__body');
-  mainbox.classList.add('dark__mainbox');
-  menu.classList.add('dark__header');
-
-  let link;
-  for(link of headerLinks){
-      link.classList.add('dark__link');
-  }
-
-
-  if (location.pathname.split("/").slice(-1) == 'home.html')
-  {
-    let genrebox = document.querySelector('.genresbtn');
-    let genreb = document.querySelectorAll('.genrechoise');
-    let slider = document.querySelectorAll('.slider')
-
-    genrebox.classList.add('dark__body');
-
-    for(link of genreb){
-      link.classList.add('dark__link');
-    }
-
-  }
-  else if(location.pathname.split("/").slice(-1) == 'top.html')
-  {
-    let titlebox = document.querySelectorAll('.titlebox');
-
-    for(box of titlebox){
-      box.classList.add('dark__titlebox');
-      box.classList.remove('titlebox__light');
-    }
-  }
-
+  body.classList.add('dark');
+  body.style.backgroundColor = '#343a40';
 }
 
 function changeTheme()
